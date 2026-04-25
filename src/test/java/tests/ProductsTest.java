68801b8e9d161d3b2a6c89adc9653fdc39dea028
OlgaToshevikova <ostoshevikova@gmail.com>
2026-04-25 23:00:41 +0400

    save

---
+++ src/test/java/tests/ProductsTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ProductsTest extends BaseTest {
+    @Test
+    public void checkProductsPage() {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        Assert.assertEquals(productsPage.checkAmountOfItemsOnPage(), 6);
+    }
+
+    public void checkChangingButtonAddTOCart() {
+        Assert.assertEquals(productsPage.checkChangesOfButtonAddToCart(), "Remove");
+    }
+}
